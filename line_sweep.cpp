37b92d824f8b68e01caee88f5d164ae51d9ebbca
Sudipta Singha <106917645+sudiptarathi2020@users.noreply.github.com>
2023-12-19 13:23:56 +0600

    Update line_sweep.cpp

--- line_sweep.cpp
+++ line_sweep.cpp
@@ -1,36 +1,74 @@
 #include<bits/stdc++.h>
-using namespace std;
 #define ll long long
-#define px second
-#define py first
-#define MAX 200001;
-const ll INF=1e18;
-#define pairll pair<long long,long long>
-ll compare(pairll a, pairll b)
-{ 
-        return a.px<b.px; 
+using pll=std::pair<ll,ll>;
+using namespace std;
+const ll Inf=1e9+1;
+struct Point{
+    ll x,y;
+    Point(){}
+    Point(ll _x,ll _y):x(_x),y(_y){}
+    bool operator<(const Point &other){
+        if(x==other.x){return y<other.y;}
+        return x<other.x;
+    }
+    bool operator>(const Point &other){
+        if(x==other.x){return y>other.y;}
+        return x>other.x;
+    }
+    bool operator==(const Point &other){
+        return x==other.x and y==other.y;
+    }
+};
+const pair<Point,Point> inf= make_pair(Point(-Inf,-Inf),Point(Inf,Inf));
+
+ll dist(const pair<Point,Point> &a){
+    ll d1=a.first.x-a.second.x;
+    ll d2=a.first.y-a.second.y;
+    return d1*d1+d2*d2;
 }
-ll closest_pair(pairll pnts[],ll n)
-{
-        sort(pnts,pnts+n,compare);
-        ll best=INF;
-        set<pairll> box;
-        box.insert(pnts[0]);
-        ll left = 0;
-        for (ll i=1;i<n;++i)
-        {
-            while (left<i && (pnts[i].px-pnts[left].px)*(pnts[i].px-pnts[left].px) > best)
-                box.erase(pnts[left++]);
-            for(typeof(box.begin()) it=box.lower_bound(make_pair(pnts[i].py-best, pnts[i].px-best));it!=box.end() && pnts[i].py+best>=it->py;it++)
-                best = min(best, (pnts[i].py-it->py)*(pnts[i].py - it->py)+(pnts[i].px - it->px)*(pnts[i].px-it->px));
-            box.insert(pnts[i]);
+pair<Point,Point> get_closest_points(const pair<Point,Point>&a,const pair<Point,Point>&b){
+    return dist(a)<=dist(b)?a:b;
+}
+pair<Point,Point> brute_forcer(vector<Point>&points){
+    pair<Point,Point>ans=inf;
+    for(int i=0;i<(int)points.size();i++){
+        for(int j=i+1;j<(int)points.size() && j-i<9;j++){
+            ans=get_closest_points(ans,make_pair(points[i],points[j]));
         }
-        return best;
+    }
+    return ans;
+}
+pair<Point,Point> solve(vector<Point>&points,int l,int r){
+    if(l==r){return inf;}
+    int mid=(l+r)>>1;
+    pair<Point,Point> ans_left=solve(points,l,mid);
+    pair<Point,Point> ans_right=solve(points,mid+1,r);
+    pair<Point,Point> ans;
+    ans=get_closest_points(ans_left,ans_right);
+    ll D=dist(ans);
+    Point mid_point=points[mid];
+    vector<Point>temp;
+    for(int i=l;i<r;i++){
+        if((points[i].x-mid_point.x)*(points[i].x-mid_point.x)<=D){
+            temp.push_back(points[i]);
+        }
+    }
+    sort(temp.begin(),temp.end(),[](Point a,Point b){
+            return a.y<b.y || (a.y==b.y && a.x< b.y);
+            });
+    return get_closest_points(ans,brute_forcer(temp));
 }
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
     int n;cin>>n;
-    pairll pnts[n];
-    for(int i=0;i<n;i++){
-        cin>>pnts[i].first>>pnts[i].second;
+    vector<Point>arr(n);
+    for(auto &X:arr){
+        cin>>X.x>>X.y;
     }
+    sort(arr.begin(),arr.end());
+    pair<Point,Point> ans=solve(arr,0,(int)arr.size());
+    cout<<dist(ans)<<endl;
+    return 0;
 }
+
