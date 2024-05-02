d3218f752ddb9e70dbb778b12e134d0120b40d6d
Sudipta Singha <106917645+sudiptarathi2020@users.noreply.github.com>
2024-05-02 15:29:50 +0600

    Created this heap for implementing priority queue.

---
+++ heap.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+template<typename T,typename Comp=std::greater<T>>struct heap{//maxheap greater<T>,minheap less<T>
+    Comp comp;
+    vector<T>hp;
+    heap(){hp.push_back(T());}
+    void push(T value){
+        hp.push_back(value);
+        up((int)hp.size()-1);
+    }
+    void up(int idx){
+        while(idx>1 && comp(hp[idx],hp[idx>>1])){
+            swap(hp[idx],hp[idx>>1]);
+            idx>>=1;
+        }
+    }
+    void pop(){
+        swap(hp[1],hp.back());
+        hp.pop_back();
+        down();
+    }
+    void down(){
+        int idx=1;
+        while((idx<<1)<(int)hp.size()){
+            int child=idx<<1;
+            if(child+1<(int)hp.size() and comp(hp[child+1],hp[child])){
+                child+=1;
+            }
+            if(comp(hp[child],hp[idx])){
+                swap(hp[child],hp[idx]);
+                idx=child;
+            }else{
+                return;
+            }
+        }
+    }
+    T peek(){return hp[1];}
+    int sz(){
+        return (int)hp.size()-1;
+    }
+};
+int main(){
+    heap<int>H;
+    for(int i=1;i<15;i+=2){
+        H.push(i);
+    }
+    while(H.sz()){
+        cout<<H.peek()<<endl;
+        H.pop();
+    }
+}
+
